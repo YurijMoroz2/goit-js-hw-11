b215fc313876f29e104816016ef7eb34e629c44f
YurijMoroz2 <139611781+YurijMoroz2@users.noreply.github.com>
2023-12-13 18:59:43 +0200

    finally hW

--- src/index.js
+++ src/index.js
@@ -1,28 +1,249 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" type="text/css" href="./style.css">
-    <!-- <link rel="stylesheet" href="dist/notiflix-3.2.6.min.css" /> -->
-        <title>Homework 11</title>
-  </head>
-  <body>
-    <form class="search-form" id="search-form">
-      <input
-        type="text"
-        name="searchQuery"
-        autocomplete="off"
-        placeholder="Search images..."
-      />
-      <button type="submit">Search</button>
-    </form>
-    <div class="gallery">
-      <!-- Картки зображень -->
-      <ul class="gallery-list">
-      </ul>
-    </div>
-    <button type="button" class="load-more">Load more</button>
-    <script src="index.js" type="module"></script>
-  </body>
-</html>
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const API_KEY = '41162617-43e33508aa3949fdb22cf209d';
+const BASE_URL = 'https://pixabay.com/api/';
+const form = document.querySelector('.search-form');
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
+const container = document.querySelector('.gallery-list');
+const galleryBox = document.querySelector('.gallery')
+// -------------------------------------------------------------------------
+let page = 1;
+let per_page = 40;
+const loadMore = document.querySelector('.load-more');
+const inputElement = document.querySelector('input[name="searchQuery"]');
+const searchForm = document.querySelector('#search-form');
+// ------------------------------------------
+galleryBox.addEventListener('click', handelClickGallery);
+let gallery
+function handelClickGallery(event) {
+  event.preventDefault();
+  //  -----------------------------------------------------------
+   gallery = new SimpleLightbox('.gallery a',{
+      spinner:'true',
+      captionsData:'alt',
+      enableKeyboard:	'true',
+    });
+    gallery.refresh();
+}
+// ---------------------------------------------------------------------------
+input.addEventListener('input', handleInput);
+function handleInput(event) {
+    resetPage()
+    btn.disabled = false
+      // console.log(event.currentTarget.value);
+  // console.log(inputElement.value);
+  if (!inputElement.value.ok) {
+    event.preventDefault();
+    container.innerHTML = '';
+    loadMore.style.display = 'none';
+      }
+    }
+    // -----------------------------------------------
+    function createMarcup(arr) {
+  return arr
+    .map(
+      ({
+        id,
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<li class ="gallery-item">
+<div class="photo-card">
+<a class ="gallery__link" href = "${largeImageURL}">        
+<img class = "gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" "/>
+</a>        
+<div class="info">
+<p class="info-item">
+<b>Likes<br>${likes}</br></b>
+</p>
+<p class="info-item">
+<b>Views<br>${views}</br></b>
+</p>
+<p class="info-item">
+<b>Comments<br>${comments}</br></b>
+</p>
+<p class="info-item">
+<b>Downloads<br>${downloads}</br></b>
+</p>
+</div>
+</div>
+</li>
+`
+    )
+    .join('');
+}
+// -----------------------------------------------
+let object;
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    container.style.listStyle = 'none';
+  const searchQuery = inputElement.value;
+  // console.log("searchQuery",searchQuery.length);
+  
+  const postData = new FormData(form);
+  const obj = Object.fromEntries(postData.entries());
+  // console.log(obj);
+  object = obj.searchQuery;
+
+  serviceTodos()
+  .then(value => {
+      // console.log(value)
+      if (value.hits.length !== 0) {
+        value.totalHits < per_page
+        ? (loadMore.style.display = 'none')
+        : (loadMore.style.display = 'block');
+        Notiflix.Notify.success(`Hooray! We found ${value.totalHits} images.`);
+      } else {
+        loadMore.style.display = 'none';
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  render()
+  .then(ren => {
+    //   console.log(ren);
+      btn.disabled = true
+        //    gallery.refresh();
+    
+    })
+}
+// ------------------1111111111111111111111111111
+async function serviceTodos(page = 1) {
+    const qweryParams = new URLSearchParams({
+        key: API_KEY,
+    q: `${object}`,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    page: page,
+    per_page: per_page,
+});
+  try {
+    const response = await axios(`${BASE_URL}?${qweryParams}`);
+        return await response.data;
+     } 
+     catch (error) {
+         console.log('Error!!!!', error);
+        }
+    }
+// -----------------------1111111111111111111111111111111111111111111111
+
+loadMore.addEventListener('click', onLoadMore);
+async function onLoadMore(event) {
+  try {
+    event.preventDefault();
+    page += 1;
+    per_page += 40;
+    const data = await serviceTodos(page);
+    // console.log("loadMore",data);
+    container.insertAdjacentHTML('beforeend', createMarcup(data.hits));
+    // console.log('page1', page);
+    // console.log('per_page onLoadMore', per_page);
+    
+    if (per_page > data.totalHits) {
+      // console.log("data.totalHits",data.totalHits);
+      Notiflix.Report.info(
+          'Info',
+          "We're sorry, but you've reached the end of search results."
+      );
+      loadMore.style.display = 'none';
+    }
+    gallery.refresh();
+} catch (error) {
+    console.log(error.message);
+  }
+}
+// -----------------------------
+async function render() {
+    try {
+        const data = await serviceTodos();
+        // console.log('render', data);
+        container.insertAdjacentHTML('beforeend', createMarcup(data.hits));
+        return await data
+    } catch (error) {
+        console.log(error.message);
+    }
+    gallery.refresh();
+}
+
+// ------------------------------------------
+
+function resetPage() {
+    page = 1; 
+    per_page = 40;
+    // console.log(page,per_page);
+  }
+// render()
+
+// key - твій унікальний ключ доступу до API.
+// q - термін для пошуку. Те, що буде вводити користувач.
+// image_type - тип зображення. На потрібні тільки фотографії, тому постав значення photo.
+// orientation - орієнтація фотографії. Постав значення horizontal.
+// safesearch - фільтр за віком. Постав значення true.
+//
+// ------------------infinity Scroll--------------------------------
+// async function infinity(){
+//     try {
+//         page +=1;
+//         const data = await serviceTodos(page)
+//         console.log("render1",data);
+//         container.insertAdjacentHTML("beforeend", createMarcup(data.hits));
+//         if(data.page<data.total){
+//             console.log("page1",data.hits.page);
+//             loadMore.classList.replace("load-more-hidden","load-more")
+//             if(data.page > data.total){
+//                 observer.observe(quard)
+//                 handlePagination()
+//             }
+//         }
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+//     }
+// const observer = new IntersectionObserver(handlePagination,options);
+// const options = {
+//     root:null,
+//     rootMargin:"100px",
+//     threshold: 0
+// }
+// 111111111111111111111111111111111111111111111111111111111
+// function handlePagination(entries,observer){
+//     entries.forEach((entry) =>{
+//         console.log(entry)
+//         if(entry.jdsahd){
+//             page+=1;
+//             async function infinity1(){
+//                 try {
+//                     page +=1;
+//                     const data = await serviceTodos(page)
+//                     console.log("render1",data);
+//                     container.insertAdjacentHTML("beforeend", createMarcup(data.hits));
+//                     if(data.page<data.total){
+//                         console.log("page1",data.hits.page);
+//                         loadMore.classList.replace("load-more-hidden","load-more")
+//                         if(data.page > data.total){
+//                             observer.unobserve(entry.target)
+//                                                     }
+//                     }
+//                 } catch (error) {
+//                     console.log(error.message);
+//                 }
+//                 }
+//         };
+//     })
+// }
